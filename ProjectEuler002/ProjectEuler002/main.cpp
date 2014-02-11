ee9d7f8dbf86c0161c6ed20ea93fbbed99716c77
arcanumghost <arcanumghost@gmail.com>
2014-02-10 22:29:10 -0800

    Problem 2

---
+++ ProjectEuler002/ProjectEuler002/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    unsigned long long sum = 2;
+    unsigned pastfib = 1;
+    unsigned pastfib2 = 2;
+    for(unsigned i=3; i<4000000; ++i)
+    {
+        i = pastfib + pastfib2;
+        pastfib = pastfib2;
+        pastfib2 = i;
+        if(i%2 == 0)
+            sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
