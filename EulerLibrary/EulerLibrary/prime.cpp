09ef84e1fa55d6737631d0c5368736e4fc1c75a7
arcanumghost <arcanumghost@gmail.com>
2014-02-24 23:23:52 -0800

    Basic Prime Sieve

--- EulerLibrary/EulerLibrary/prime.cpp
+++ EulerLibrary/EulerLibrary/prime.cpp
@@ -1,19 +1,28 @@
 #include "prime.h"
-#include <vector>
 
-bool isPrime(unsigned x)
+PrimeFinder::PrimeFinder(unsigned startMax)
+    : max(0)
 {
-    static std::vector<int> primevalues;
-    
-    unsigned i;
-    for(i=0; i<primevalues.size(); ++i)
-        if(x % primevalues[i] == 0)
-            return false;
+    calcPrimes(startMax);
+}
 
-    for(i = (primevalues.size() > 1 ? primevalues[i-1] : 2); i < x; ++i)
-        if(x%i == 0)
-            return false;
+bool PrimeFinder::isPrime(unsigned x)
+{
+    if(x < 2)
+        return false;
+    if(x == 2)
+        return true;
+    return !sieve[(x-3)>>1];
+}
 
-    primevalues.push_back(x);
-    return true;
+// Initially, this function resets the primes and starts over
+void PrimeFinder::calcPrimes(unsigned newMax)
+{
+    unsigned sieveBound = (newMax-1)>>1;
+    sieve.resize(sieveBound, false);
+    unsigned crosslimit = (unsigned(floor(sqrt(newMax))-1))>>1;
+    for(unsigned i=0; i<crosslimit; ++i)
+        if(!sieve[i])
+            for(unsigned j=(2*(i+1)*(i+2)); j<=sieveBound; j+=(2*i+3))
+                sieve[j-1] = true;
 }
--- EulerLibrary/EulerLibrary/prime.h
+++ EulerLibrary/EulerLibrary/prime.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <vector>
 
 // Current function requires that it be previously fed each number
-bool isPrime(unsigned x);
+class PrimeFinder
+{
+public: 
+    bool isPrime(unsigned x);    
+    PrimeFinder(unsigned startMax);
+
+private:
+    void calcPrimes(unsigned max = UINT_MAX);
+    std::vector<bool> sieve;
+    unsigned max;
+};
